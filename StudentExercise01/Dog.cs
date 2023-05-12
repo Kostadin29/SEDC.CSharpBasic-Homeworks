11aff0e8529024bf8bf8ea226cb0adef79b79b7a
Kostadin <117443351+Kostadin29@users.noreply.github.com>
2023-05-12 17:51:09 +0200

    Added Student Exercise - C# Advanced

---
+++ StudentExercise01/Dog.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+
+namespace StudentExercise01
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Color { get; set; }
+
+        public Dog()
+        {
+            
+        }
+    }
+}
---
+++ StudentExercise01/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+using Newtonsoft.Json;
+using StudentExercise01;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+
+string folderPath = @"..\..\..\StudentExerciseJson";
+string filePath = folderPath + @"\dogJson.json";
+
+
+Console.WriteLine("Please enter a dog name");
+string dogName = Console.ReadLine();
+Console.WriteLine("Please enter a dog age");
+int dogAge = int.Parse(Console.ReadLine());
+Console.WriteLine("Please enter a dog color");
+string dogColor = Console.ReadLine();
+
+Dog dog = new Dog() { Name = dogName, Age = dogAge, Color = dogColor };
+
+if (!Directory.Exists(folderPath))
+{
+    Directory.CreateDirectory(folderPath);
+}
+
+if (!File.Exists(filePath))
+{
+    File.Create(filePath).Close();
+}
+
+string jsonDog = JsonConvert.SerializeObject(dog);
+
+ReaderWriter.WriteFile(filePath, jsonDog);
+
+string jsonData = ReaderWriter.ReadFile(filePath);
+Dog deserializedDog = JsonConvert.DeserializeObject<Dog>(jsonData);
+Console.WriteLine(deserializedDog.Name + " " + deserializedDog.Age + " " + deserializedDog.Color);
